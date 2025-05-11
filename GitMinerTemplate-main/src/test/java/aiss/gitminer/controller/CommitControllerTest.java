cea6417c7a4cad3cd7e136c7c4782150024b9aee
FDK4592 <sanperriv@alum.us.es>
2025-05-11 14:31:56 +0200

    GitMiner Completo con todos los test pasados.

---
+++ GitMinerTemplate-main/src/test/java/aiss/gitminer/controller/CommitControllerTest.java
@@ -0,0 +1,16 @@
+package aiss.gitminer.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommitControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
