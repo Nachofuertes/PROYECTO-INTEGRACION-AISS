511b301d45bc6790723df9a87716cb6a624a0085
fernandopinedaa <ferpinmun@alum.us.es>
2025-05-11 23:02:24 +0200

    pitpocket_update.4

--- BitBucketMiner-main/src/main/java/aiss/BitBucketMiner/config/RestTemplateConfig.java
+++ BitBucketMiner-main/src/main/java/aiss/BitBucketMiner/config/RestTemplateConfig.java
@@ -1,15 +1,26 @@
-// RestTemplateConfig.java en el paquete config
 package aiss.BitBucketMiner.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
+
+    @Value("${bitbucket.api.username}")
+    private String username;
+
+    @Value("${bitbucket.api.token}")
+    private String password;
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        return builder
+                .basicAuthentication(username, password)
+                .build();
     }
 }
